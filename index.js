1415353f326b4eab9eee8f9043c96ecf00793c01
PowfuArras <jekyll.arw@gmail.com>
2023-05-11 05:51:27 -0500

    refactoring

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const concurrently = require("concurrently");
-const commands = {};
-
-commands.run = [{
-    "name": "polybox-client",
-    "command": "\"cd ./polybox-client && npm run server\""
-}, {
-    "name": "polybox-server",
-    "command": "\"cd ./polybox-server && npm run server\""
-}];
-
-commands.build = [{
-    "name": "client-host",
-    "command": "\"cd ./polybox-client && npm run build-server\""
-}, {
-    "name": "client-webpack",
-    "command": "\"cd ./polybox-client && npm run build-client\""
-}, {
-    "name": "server",
-    "command": "\"cd ./polybox-server && npm run build\""
-}];
-
-commands.watch = [{
-    "name": "client-host",
-    "command": "\"cd ./polybox-client && npm run watch-server\""
-}, {
-    "name": "client-webpack",
-    "command": "\"cd ./polybox-client && npm run watch-client\""
-}, {
-    "name": "server",
-    "command": "\"cd ./polybox-server && npm run watch\""
-}];
+const commands = {
+    "run": [{
+        "name": "polybox-server",
+        "command": "\"cd ./polybox-server && npm run server\""
+    }, {
+        "name": "polybox-client",
+        "command": "\"cd ./polybox-client && npm run server\""
+    }],
+    "build": [{
+        "name": "client-host",
+        "command": "\"cd ./polybox-client && npm run build-server\""
+    }, {
+        "name": "client-webpack",
+        "command": "\"cd ./polybox-client && npm run build-client\""
+    }, {
+        "name": "server",
+        "command": "\"cd ./polybox-server && npm run build\""
+    }],
+    "watch": [{
+        "name": "client-host",
+        "command": "\"cd ./polybox-client && npm run watch-server\""
+    }, {
+        "name": "client-webpack",
+        "command": "\"cd ./polybox-client && npm run watch-client\""
+    }, {
+        "name": "server",
+        "command": "\"cd ./polybox-server && npm run watch\""
+    }]
+};
 
 const key = process.argv[2]?.split("-")?.[1] ?? "run";
 if (!commands.hasOwnProperty(key)) throw new Error(`Job "${key}" is not valid.`);
